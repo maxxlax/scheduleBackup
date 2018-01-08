d54762c91a814242a0822ecefe497781fd9b5255
Max Croston <maxxlax@gmail.com>
2018-01-08 18:16:03 -0500

    Final Test

--- ScheduleMaker/src/gui/MyDragDropList.java
+++ ScheduleMaker/src/gui/MyDragDropList.java
@@ -13,81 +13,81 @@ import employee.Employee;
 @SuppressWarnings("serial")
 public class MyDragDropList extends JList<String> implements KeyListener
 {
-	private DefaultListModel<String> model;
-	private int currentSelected = -1;
+  private DefaultListModel<String> model;
+  private int currentSelected = -1;
 
-	//Yo
-	public MyDragDropList()
-	{
-		super(new DefaultListModel<String>());
-		addKeyListener(this);
-		model = (DefaultListModel<String>) getModel();
-		
-		addListSelectionListener(new ListSelectionListener()
-		{
-			@Override
-			public void valueChanged(ListSelectionEvent lse)
-			{
-				currentSelected = getSelectedIndex();
-			}
+  public MyDragDropList()
+  {
+    super(new DefaultListModel<String>());
+    addKeyListener(this);
+    model = (DefaultListModel<String>) getModel();
 
-		});
-	}
+    addListSelectionListener(new ListSelectionListener()
+    {
+      @Override
+      public void valueChanged(ListSelectionEvent lse)
+      {
+        currentSelected = getSelectedIndex();
+      }
 
-	public void addShift(String str)
-	{
-		model.addElement(str);
-	}
-	public void addEmployee(Employee emp)
-	{
-		model.addElement(emp.fullName);
-	}
+    });
+  }
 
-	public void removeEmployee(Employee emp)
-	{
-		model.removeElement(emp.fullName);
-	}
+  public void addShift(String str)
+  {
+    model.addElement(str);
+  }
 
-	@Override
-	public void keyPressed(KeyEvent ke)
-	{
-		if (currentSelected != -1)
-		{
-			String currentString = model.get(currentSelected);
-			if (ke.getKeyCode() == 38)
-			{
-				if (currentSelected != 0)
-				{
-					int c = currentSelected;
-					model.remove(c);
-					model.insertElementAt(currentString, c - 1);
-				}
-			}
-			if (ke.getKeyCode() == 40)
-			{
-				if(currentSelected != model.size() - 1)
-				{
-					int c = currentSelected;
-					model.remove(c);
-					model.insertElementAt(currentString, c + 1);
-					setSelectedIndex(c);
-				}
-			}
-		}
-	}
+  public void addEmployee(Employee emp)
+  {
+    model.addElement(emp.fullName);
+  }
 
-	@Override
-	public void keyReleased(KeyEvent arg0)
-	{
-	}
+  public void removeEmployee(Employee emp)
+  {
+    model.removeElement(emp.fullName);
+  }
 
-	@Override
-	public void keyTyped(KeyEvent ke)
-	{
-	}
+  @Override
+  public void keyPressed(KeyEvent ke)
+  {
+    if (currentSelected != -1)
+    {
+      String currentString = model.get(currentSelected);
+      if (ke.getKeyCode() == 38)
+      {
+        if (currentSelected != 0)
+        {
+          int c = currentSelected;
+          model.remove(c);
+          model.insertElementAt(currentString, c - 1);
+        }
+      }
+      if (ke.getKeyCode() == 40)
+      {
+        if (currentSelected != model.size() - 1)
+        {
+          int c = currentSelected;
+          model.remove(c);
+          model.insertElementAt(currentString, c + 1);
+          setSelectedIndex(c);
+        }
+      }
+    }
+  }
 
-	public void removeEmployee(String string)
-	{
-		model.removeElement(string);
-	}
+  @Override
+  public void keyReleased(KeyEvent arg0)
+  {
+  }
+
+  @Override
+  public void keyTyped(KeyEvent ke)
+  {
+  }
+
+  public void removeEmployee(String string)
+  {
+    model.removeElement(string);
+  }
 }
